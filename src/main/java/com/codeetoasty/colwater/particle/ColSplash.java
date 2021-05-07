e368605a742c14b453ad5836e019cce2a37e6c20
CodeeToasty <tosted94@gmail.com>
2021-05-07 16:40:11 +0200

    Splashes and Bubbles are now more saturated

--- src/main/java/com/codeetoasty/colwater/particle/ColSplash.java
+++ src/main/java/com/codeetoasty/colwater/particle/ColSplash.java
@@ -12,9 +12,12 @@ import net.minecraft.particle.DefaultParticleType;
 
 @Environment(EnvType.CLIENT)
 public class ColSplash extends RainSplashParticle {
-    protected ColSplash(ClientWorld world, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
+    protected ColSplash(ClientWorld world, double x, double y, double z, double velocityX, double velocityY, double velocityZ,float red, float green, float blue) {
         super(world, x, y, z);
         this.gravityStrength = 0.04F;
+        this.colorRed = red;
+        this.colorGreen = green;
+        this.colorBlue = blue;
         if (velocityY == 0.0D && (velocityX != 0.0D || velocityZ != 0.0D)) {
             this.velocityX = velocityX;
             this.velocityY = 0.1D;
@@ -26,14 +29,20 @@ public class ColSplash extends RainSplashParticle {
     @Environment(EnvType.CLIENT)
     public static class SplashFactory implements ParticleFactory<DefaultParticleType> {
         private final SpriteProvider spriteProvider;
+        private final float red;
+        private final float green;
+        private final float blue;
 
-        public SplashFactory(SpriteProvider spriteProvider) {
+        public SplashFactory(SpriteProvider spriteProvider, float red, float green, float blue) {
             this.spriteProvider = spriteProvider;
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
         }
 
 
         public Particle createParticle(DefaultParticleType defaultParticleType, ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
-            ColSplash testSplash = new ColSplash(clientWorld, d, e, f, g, h, i);
+            ColSplash testSplash = new ColSplash(clientWorld, d, e, f, g, h, i, red, green, blue);
             testSplash.setSprite(this.spriteProvider);
             return testSplash;
         }
